505b40d7724c5dd96f1f8a1149f0b2943e884271
zhiwu <zhiwu828@sina.com>
2019-11-20 00:22:22 +0800

    ch1-dup1

---
+++ theGoProgLang/CH01/Dup1/dup1.go
@@ -0,0 +1,24 @@
+//运行go run dup1.go 然后每行输入字符，需要退出时输入end 。
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		if input.Text() == "end" {
+			break
+		} //控制程序退出
+		counts[input.Text()]++
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
