3309f45bdb2a7090cb30a73dfa53531501fa1f05
zhiwu <zhiwu828@sina.com>
2019-12-21 00:22:38 +0800

    ch1-fetch-fetch

--- theGoProgLang/CH01/lissajous/lissajous.go
+++ theGoProgLang/CH01/lissajous/lissajous.go
@@ -1,4 +1,5 @@
 //lissajous产生随机利萨茹图形的GIF动画
+//执行 go run lissajous.go web ,执行后监听本地8080端口，从浏览器打开 http://127.0.0.1:8080/ 可以看到生成的图像。
 package main
 
 import (
@@ -14,7 +15,8 @@ import (
 	"time"
 )
 
-var palette = []color.Color{color.White, color.Black}
+//var palette = []color.Color{color.White, color.Black}  //切片里存黑白两种颜色，切片是color类型
+var palette = []color.Color{color.RGBA{0x00, 0x22, 0x33, 0xff}, color.Black}
 
 const (
 	whiteIndex = 0 //画板中的第一种颜色
@@ -47,15 +49,15 @@ func lissajous(out io.Writer) {
 	phase := 0.0
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
+		img := image.NewPaletted(rect, palette) //使用颜色和尺寸创建图
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex) //每次循环设置一些像素为黑色
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)
+		anim.Image = append(anim.Image, img) //将图像追加到GIF帧列表中
 	}
 	gif.EncodeAll(out, &anim)
 }
