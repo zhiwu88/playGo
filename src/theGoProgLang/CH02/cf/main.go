699d75ffdcd6aeef41eccb1d5b6a86e675de78be
zhiwu <zhiwu828@sina.com>
2020-01-10 23:58:41 +0800

    CH02-cf

--- src/theGoProgLang/CH02/cf/main.go
+++ src/theGoProgLang/CH02/cf/main.go
@@ -5,6 +5,14 @@ import "fmt"
 import tempconv "theGoProgLang/CH02/tempconv"
 
 func main()  {
-	f := tempconv.CToF(1)
-	fmt.Println(f)
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+		f := tempconv.Fahrenheit(t)
+		c := tempconv.Celsius(t)
+		fmt.Printf("%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+	}
 }
